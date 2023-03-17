371bbfb8b5e804e815cbb200ab2dd335fa44ebe0
taemin-Jang <jtm990506@gmail.com>
2023-03-17 21:32:27 +0900

    (#2) test : naver search api 동작 테스트

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,29 @@
 import express, { Application, Request, Response } from "express";
+import axios, { AxiosResponse } from "axios";
+import config from "@config";
 
 const app: Application = express();
-const port: number = 3000;
+const port = config.port;
 
 app.get("/", async (req: Request, res: Response) => {
-  res.send("hello");
+  try {
+    const headers = {
+      "X-Naver-Client-Id": config.naver_client_id,
+      "X-Naver-Client-Secret": config.naver_client_secret,
+      Accept: "*/*",
+      "User-Agent": "curl/7.49.1",
+      Host: config.naver_host,
+    };
+
+    const aixosResponse: AxiosResponse = await axios.get(
+      "https://openapi.naver.com/v1/search/news.json?query=WBC",
+      { headers }
+    );
+    console.log(aixosResponse.data);
+    res.status(200).send(aixosResponse.data);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(port, () => {
--- src/config/index.ts
+++ src/config/index.ts
@@ -9,6 +9,7 @@ export default {
 
   naver_client_id: process.env.NAVER_CLIENT_ID as string,
   naver_client_secret: process.env.NAVER_CLIENT_SECRET as string,
+  naver_host: process.env.NAVER_HOST as string,
 
   openai_api_key: process.env.OPENAI_API_KEY as string,
 };

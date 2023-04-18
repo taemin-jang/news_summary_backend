edb8b50c00e9a7e2defcde1643f1b8002f598501
taemin-Jang <jtm990506@gmail.com>
2023-04-18 23:48:23 +0900

    loaders/index loaders 진입점

---
+++ src/loaders/index.ts
@@ -0,0 +1,15 @@
+import expressLoader from "./express";
+import { Application } from "express";
+import dependencyInjector from "./dependencyInjector";
+import LoggerInstance from "./logger";
+import sequelize from "./sequelize";
+
+export default async (expressApp: Application) => {
+  const sequelizeInstance = await sequelize();
+  LoggerInstance.info("DB Loaded");
+
+  await dependencyInjector(sequelizeInstance);
+
+  await expressLoader(expressApp);
+  LoggerInstance.info("Express Loaded");
+};

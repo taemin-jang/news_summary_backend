2682d6a2666be71a64e12fb87074f7e20bbafa2a
taemin-Jang <jtm990506@gmail.com>
2023-04-25 20:49:40 +0900

    fix : naver api

--- src/api/routes/naverSearch.ts
+++ src/api/routes/naverSearch.ts
@@ -3,11 +3,15 @@ import Container from "typedi";
 import { Logger } from "winston";
 import config from "@config";
 import axios, { AxiosResponse } from "axios";
+import { getArticle } from "@services/articleURL";
 
 export default (app: Router) => {
   const logger: Logger = Container.get("logger");
 
-  // naver api test
+  /**
+   * naver search api로 뉴스 기사를 받음
+   * 각 기사에 이미지들을 추가하고 네이버 뉴스만 가져와 반환
+   */
   app.get("/naver/:keyword", async (req: Request, res: Response) => {
     try {
       const headers = {
@@ -17,12 +21,19 @@ export default (app: Router) => {
         "User-Agent": "curl/7.49.1",
         Host: config.naver_host,
       };
-
+      // naver search api 호출
       const aixosResponse: AxiosResponse = await axios.get(
-        `https://openapi.naver.com/v1/search/news.json?query=${req.params.keyword}`,
+        `https://openapi.naver.com/v1/search/news.json?query=${req.params.keyword}&display=20`,
         { headers }
       );
-      res.status(200).send(aixosResponse.data);
+      // 네이버 뉴스 기사 저장
+      let articles = "";
+      // 뉴스 기사에 이미지를 추가하고 네이버 뉴스 기사만 articles에 저장
+      await getArticle(aixosResponse.data.items, 0).then(
+        (result) =>
+          (articles = result.filter((article) => article.images !== null))
+      );
+      res.status(200).send(articles);
     } catch (err) {
       logger.error(err);
     }

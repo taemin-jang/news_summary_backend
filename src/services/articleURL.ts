103fdf25a165eaae5230b4fb28448d053336abe0
taemin-Jang <jtm990506@gmail.com>
2023-04-25 20:46:46 +0900

    feat : 뉴스 기사 중 네이버 뉴스 기사만 추출 후 이미지 링크 삽입

---
+++ src/services/articleURL.ts
@@ -0,0 +1,33 @@
+import { getImage } from "@services/imageFromArticle";
+/**
+ * 네이버 기사 정보에 image를 추가해서 반환하는 함수
+ * @param articleArray 네이버 search api로 받아온 뉴스 기사
+ * @param id 해당 기사의 인덱스
+ * @returns articleArray
+ */
+export const getArticle = async (articleArray, id) => {
+  // news.naver가 포함된 주소만 찾는 정규식
+  const regex = /news\.naver/;
+  // 기사 URL
+  let articleURL = "";
+
+  if (id < articleArray.length) {
+    // news.naver일 경우
+    if (regex.test(articleArray[id].link)) {
+      articleURL = articleArray[id].link;
+      // 해당 기사 정보 요청
+      const response = await fetch(articleURL);
+      // 기사의 htmlText 요청
+      const htmlText = await response.text();
+      // htmlText에 있는 이미지
+      const image = getImage(htmlText);
+      // 이미지를 뉴스 기사에 추가
+      articleArray[id].images = image;
+    } else {
+      // 네이버 뉴스 기사가 아닌경우 images에 null 추가
+      articleArray[id].images = null;
+    }
+    await getArticle(articleArray, id + 1);
+  }
+  return articleArray;
+};

2a4c4e396288a17b89711ac53974e49dd08b0624
taemin-Jang <jtm990506@gmail.com>
2023-05-10 17:21:18 +0900

    api 경로로 들어온 req.body 값이 json으로 파싱되지 않는 문제 해결

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -8,12 +8,13 @@ export default (app: express.Application) => {
 
   // X-Powered-By 막기 => X-Powered-By는 어떤 프레임워크와 기술을 이용했는지 알 수 있으므로 안보이도록 설정
   app.disable("x-powered-by");
+  // express.json으로 클라이언트에서 전송되는 데이터를 JSON으로 파싱
+  app.use(express.json());
+  // 클라이언트가 URL 인코딩된 데이터를 전송할 경우
+  app.use(express.urlencoded({ extended: true }));
   // /api 경로로 들어오면 api 폴더에 맞게 라우팅
   app.use("/api", routes());
   app.use(history());
-
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
 
   app.use((req, res, next) => {
     const err: any = new Error("Not Found");

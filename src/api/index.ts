9c3dcdfa5b07cd81e0c94a751cc103275b5a1323
taemin-Jang <jtm990506@gmail.com>
2023-05-10 17:17:22 +0900

    분리된 route들을 관리

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import naverSearch from "./routes/naverSearch";
 import chatGPT from "./routes/chatGPT";
+import kakao from "./routes/kakao";
 
 export default () => {
   const app = Router();
   naverSearch(app);
   chatGPT(app);
+  kakao(app);
   return app;
 };

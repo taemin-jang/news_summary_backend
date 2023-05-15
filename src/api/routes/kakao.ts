ce121b69ea11f1c032fb2e7c7f0071ba5577984e
taemin-Jang <jtm990506@gmail.com>
2023-05-15 17:46:33 +0900

    kakaoProfile정보 session.user에 추가

--- src/api/routes/kakao.ts
+++ src/api/routes/kakao.ts
@@ -6,10 +6,11 @@ import KakaoService from "@services/kakaoProfile";
 export default (app: Router) => {
   const logger: Logger = Container.get("logger");
 
-  app.post("/kakao-login", async (req, res) => {
+  app.post("/kakao-login", async (req: Request, res: Response) => {
     try {
       const kakaoInstance = new KakaoService();
       const response = await kakaoInstance.getProfile(req.body.access_token);
+      req.session.user = response.dataValues;
       res.status(200).json(response);
     } catch (error) {
       logger.error(error);

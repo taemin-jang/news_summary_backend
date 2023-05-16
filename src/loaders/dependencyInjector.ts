4fbaf505f9001bfc4b7ae0388739910174193a54
taemin-Jang <jtm990506@gmail.com>
2023-05-16 20:53:19 +0900

    session 의존성 제거

--- src/loaders/dependencyInjector.ts
+++ src/loaders/dependencyInjector.ts
@@ -1,7 +1,6 @@
 import { Sequelize } from "sequelize";
 import { Container } from "typedi";
 import LoggerInstance from "./logger";
-import SessionInstance from "./session";
 import models from "@models/index";
 import Kakao from "@models/Kakao";
 
@@ -12,9 +11,6 @@ export default async (sequelizeInstance: Sequelize) => {
 
   // db 의존성 설정
   Container.set("db", sequelizeInstance);
-
-  // session 의존성 설정
-  Container.set("session", SessionInstance);
 
   // 테이블 생성
   models(sequelizeInstance);

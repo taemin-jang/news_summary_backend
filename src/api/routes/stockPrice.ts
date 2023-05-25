c739a4514d711639cab04ab40926f3dc113f31ab
taemin-Jang <jtm990506@gmail.com>
2023-05-26 00:20:20 +0900

    stock routing

--- src/api/routes/stockPrice.ts
+++ src/api/routes/stockPrice.ts
@@ -1,24 +1,22 @@
 import { Router, Request, Response } from "express";
 import Container from "typedi";
 import { Logger } from "winston";
-import axios, { AxiosResponse } from "axios";
-import config from "@config";
-import moment from "moment";
+import StockService from "@services/stockList";
 
 export default (app: Router) => {
   const logger: Logger = Container.get("logger");
-  const data = moment();
+  const StockInstance = new StockService();
+
   // 키워드로 주식 정보 요청 시 주식 정보 반환
   app.get("/stock", async (req: Request, res: Response) => {
     try {
-      const response: AxiosResponse = await axios.get(
-        `${config.stock_base_url}/getStockPriceInfo?serviceKey=${
-          config.stock_service_key
-        }&basDt=${data.format("YYYYMMDD")}&itmsNm=${
-          req.query.search
-        }&resultType=json`
-      );
-      res.json(response.data.response.body.items);
+      if (req.query.search !== undefined) {
+        const response = await StockInstance.getStock(
+          req.query.search.toString()
+        );
+        // console.log(response.data.response.body.items);
+        res.json(response.data.response.body.items);
+      }
     } catch (err) {
       logger.error(err);
     }

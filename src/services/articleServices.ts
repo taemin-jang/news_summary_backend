f8acf23253a032ddd3a22615bff7e6c2d9a02577
taemin-Jang <jtm990506@gmail.com>
2023-05-31 03:20:17 +0900

    addArticleInfo(), getUserArticle() 추가

--- src/services/articleServices.ts
+++ src/services/articleServices.ts
@@ -2,12 +2,18 @@ import Container from "typedi";
 import { Logger } from "winston";
 import { ModelCtor, Sequelize } from "sequelize";
 import { NaverNewsResponse } from "../types/NaverNewsResponse";
+import { getContent } from "@services/articleContent";
+import { getImage } from "@services/imageFromArticle";
+import { getContentSummary } from "@services/contentSummary";
+import { getGptKeyword } from "@services/gptKeyword";
 
 export default class ArticleService {
   articleModle: ModelCtor<any>;
+  portfolioModel: ModelCtor<any>;
   constructor() {
     const db: Sequelize = Container.get("db");
     this.articleModle = db.models.Article;
+    this.portfolioModel = db.models.Portfolio;
   }
 
   /**
@@ -31,5 +37,83 @@ export default class ArticleService {
       const logger: Logger = Container.get("logger");
       logger.error(err);
     }
+  }
+
+  public async addArticleInfo(
+    articleArray: NaverNewsResponse[],
+    id: number,
+    keyword: string
+  ) {
+    // news.naver가 포함된 주소만 찾는 정규식
+    const regex = /news\.naver/;
+    // 기사 URL
+    let articleURL: string = "";
+
+    if (id < articleArray.length) {
+      // news.naver일 경우
+      if (regex.test(articleArray[id].link)) {
+        articleURL = articleArray[id].link;
+        // 해당 기사 정보 요청
+        const response = await fetch(articleURL);
+        // 기사의 htmlText 요청
+        const htmlText = await response.text();
+        // htmlText에 있는 이미지
+        const image = getImage(htmlText);
+        if (image !== null) {
+          // 이미지를 뉴스 기사에 추가
+          articleArray[id].images = image;
+          const content = getContent(htmlText);
+          // 기사 내용 추가
+          articleArray[id].content = content;
+
+          if (content !== null) {
+            const contentSummary = await getContentSummary(
+              content,
+              articleArray[id].title
+            );
+            if (contentSummary.summary) {
+              // 기사 요약본 추가
+              articleArray[id].summary = contentSummary.summary;
+              // 기사 핵심 키워드 추가
+              const keywordRes = await getGptKeyword(contentSummary.summary);
+              const keywords = keywordRes
+                ?.split("\n")
+                .map((keyword) => keyword.split(". ")[1]);
+              articleArray[id].keywords = keywords;
+
+              // 기사에 추가한 내용 db 등록
+              await this.registArticle(articleArray[id], keyword);
+            } else articleArray[id].summary = contentSummary.error;
+          }
+        }
+      } else {
+        // 네이버 뉴스 기사가 아닌경우 images에 null 추가
+        articleArray[id].images = null;
+      }
+      await this.addArticleInfo(articleArray, id + 1, keyword);
+    }
+
+    return articleArray;
+  }
+
+  public async getUserArticle(user_id) {
+    const myPortfolio = await this.portfolioModel.findAll({
+      where: {
+        kakao_id: user_id,
+      },
+    });
+    let arr: any = new Map();
+    for (let i in myPortfolio) {
+      arr.set(
+        myPortfolio[i].stock_id,
+        await this.articleModle.findAll({
+          where: {
+            stock_id: myPortfolio[i].stock_id,
+          },
+          limit: 5,
+        })
+      );
+    }
+    return arr;
   }
 }
